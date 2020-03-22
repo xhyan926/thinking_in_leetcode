fd69fcd743d3112a7e111c284febe4095c935079
yanliwei <xhyan926@gmail.com>
2020-03-22 22:19:23 +0800

    两数之和

---
+++ src/com/xhyan/leetcode/P_01_TwoSum.java
@@ -0,0 +1,24 @@
+package com.xhyan.leetcode;
+
+public class P_01_TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
